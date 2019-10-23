d3036156cc68367fbd8f71774ac4a52613abbff0
github username <github email address>
2019-10-22 17:29:04 -0700

    Added comment class

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
-    belongs_to :assignee
-    belongs_to :assigner 
+    belongs_to :assignee, foreign_key: "assignee_id", class_name: "User"
+    belongs_to :assigner, foreign_key: "assigner_id", class_name: "User" 
+    has_many :tasks
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     validates :username, uniqueness: true
     
     has_many :task_assignees, foreign_key: :assigner_id, class_name: "User"
-	has_many :asignees, through: :task_assignees
+	has_many :assignees, through: :task_assignees
     
     has_many :task_assigners, foreign_key: :assignee_id, class_name: "User"
     has_many :assigners, through: :task_assigners
